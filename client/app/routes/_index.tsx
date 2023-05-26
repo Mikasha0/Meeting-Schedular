4ec2814e59f8c352ccb348ca80d48c3f00c54039
Mikasha0 <anikettamrakar0@gmail.com>
2023-05-26 13:02:12 +0545

    added a image folder and added yarsa cube image inside it

---
+++ client/app/routes/_index.tsx
@@ -0,0 +1,11 @@
+export default function index() {
+  return (
+    <>
+      <main className="min-h-screen bg-black flex justify-center items-center">
+        <div className="bg-red-500">
+          <p>I love you</p>
+        </div>
+      </main>
+    </>
+  );
+}
--- client/app/routes/index.tsx
+++
@@ -1,8 +0,0 @@
-export default function index() {
-  return (
-    <>
-      <div className="text-red-600">index</div>
-      <h1 className="text-white">tailwindcss</h1>
-    </>
-  );
-}
