9acf6cc69468dbad7d93bce7b3812aa759259f99
Mikasha0 <anikettamrakar0@gmail.com>
2023-05-26 14:04:19 +0545

    made some design changes for the cal-clone

--- client/app/routes/_index.tsx
+++ client/app/routes/_index.tsx
@@ -1,10 +1,20 @@
+import yarsa_cube from "~/images/yarsa-cube-grey.svg";
+
 export default function index() {
   return (
     <>
-      <main className="min-h-screen bg-black flex justify-center items-center">
-        <div className="bg-red-500">
-          <p>I love you</p>
+      <main className="min-h-screen bg-black flex flex-col justify-center items-center">
+        <div className="text-white">
+          <img
+            src={yarsa_cube}
+            alt="yarsa_logo"
+            style={{ width: "6rem", height: "6rem" }}
+          />
         </div>
+        <div className="font-col text-white text-3xl mb-1">Yarsa Labs</div>
+        <span className="text-gray-500 font-medium">
+          Book your time at our Pokhara or Kathmandu offices.
+        </span>
       </main>
     </>
   );
