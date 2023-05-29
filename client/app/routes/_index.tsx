93ff71f19686544631c623d43ba4407edf8e9bf5
Mikasha0 <anikettamrakar0@gmail.com>
2023-05-29 08:57:04 +0545

    font size changes

--- client/app/routes/_index.tsx
+++ client/app/routes/_index.tsx
@@ -1,33 +1,61 @@
+import type { LinksFunction } from "@remix-run/node";
 import yarsa_cube from "~/images/yarsa-cube-grey.svg";
+import stylesheet from "~/styles/index.css";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesheet },
+];
 
 export default function index() {
   return (
     <>
       <main className="min-h-screen bg-black flex flex-col items-center">
-        <div className="text-white" style={{ marginTop: "8rem" }}>
-          <img
-            src={yarsa_cube}
-            alt="yarsa_logo"
-            style={{ width: "6rem", height: "6rem" }}
-          />
+        <div className="text-white" style={{ marginTop: "7rem" }}>
+          <div
+            className="logo-container "
+            style={{
+              width: "7rem",
+              height: "7rem",
+              borderRadius: "50%",
+              overflow: "hidden",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              boxShadow: "0 0 10px rgba(0.9, 0.9, 0.9, 0.9)",
+              backgroundColor: "#1c1c1c",
+            }}
+          >
+            <img
+              src={yarsa_cube}
+              alt="yarsa_logo"
+              style={{ width: "100%", height: "100%" }}
+            />
+          </div>
         </div>
+
         <div className="font-col text-white text-3xl mb-1 font-bold">
           Yarsa Labs
         </div>
-        <span className="text-gray-500 font-medium">
+        <span className="text-gray-500 fontSize">
           Book your time at our Pokhara or Kathmandu offices.
         </span>
 
         <div
-          className="bg-white rounded-lg shadow-lg p-4 mt-5"
-          style={{ maxWidth: "700px", width: "100%" }}
+          className=" rounded-lg shadow-lg p-4 mt-7"
+          style={{
+            maxWidth: "600px",
+            width: "100%",
+            backgroundColor: "#1C1C1C",
+          }}
         >
-          <h2 className="text-sm font-semibold pr-2">30 Min Meeting</h2>
-          <h2 className="text-sm pr-2">A 30 minutes meeting.</h2>
+          <h2 className="text-1xl font-semibold pr-2 text-white">
+            30 Min Meeting
+          </h2>
+          <h2 className="text-1xl pr-2 text-white">A 30 minutes meeting.</h2>
 
           <ul className="mt-2 flex flex-wrap gap-x-2 gap-y-1">
             <li>
-              <div className="font-medium inline-flex items-center justify-center rounded gap-x-1 bg-subtle text-emphasis py-1 px-1.5 text-xs leading-3">
+              <div className="font-medium inline-flex items-center justify-center rounded gap-x-1 bg-subtle text-emphasis py-1 px-1.5 text-xs leading-3 text-white">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="24"
@@ -38,7 +66,7 @@ export default function index() {
                   stroke-width="2"
                   stroke-linecap="round"
                   stroke-linejoin="round"
-                  className="h-3 w-3 stroke-[3px]"
+                  className="h-3 w-3 stroke-[3px] text-white"
                 >
                   <circle cx="12" cy="12" r="10"></circle>
                   <polyline points="12 6 12 12 16 14"></polyline>
