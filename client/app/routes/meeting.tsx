ff9eba477d747bafdd7fcd112f9ab411dcadc436
Mikasha0 <anikettamrakar0@gmail.com>
2023-05-31 14:03:14 +0545

    current changes commited

--- client/app/routes/meeting.tsx
+++ client/app/routes/meeting.tsx
@@ -4,8 +4,13 @@ import Calendar from "react-calendar";
 export default function Meeting() {
   const [value, onChange] = useState(new Date());
   return (
-    <div style={{ width: "40%", height: "40%" }}>
-      <Calendar value={value} />
-    </div>
+    // <div style={{ width: "40%", height: "40%" }}>
+    // </div>
+    <main className="calendar min-h-screen bg-black flex items-center justify-center">
+      <Calendar
+        className="text-white bg-gray-900 p-4 rounded-lg shadow-lg"
+        value={value}
+      />
+    </main>
   );
 }
