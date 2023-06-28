05e3118230d7d1d8bf8ea2778588e4388ad56484
Aniket Tamrakar <anikettamrakar0@gmail.com>
2023-06-28 01:23:12 -0700

    installed react icons library

--- client/app/routes/meeting.tsx
+++ client/app/routes/meeting.tsx
@@ -1,5 +1,7 @@
 import { Menu, Transition } from "@headlessui/react";
 // import { DotsVerticalIcon } from "@heroicons/react/24/outline";
+import yarsa_cube from "~/images/yarsa-cube-grey.svg";
+
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/solid";
 import { LinksFunction } from "@remix-run/node";
 import {
@@ -21,49 +23,6 @@ import stylesheet from "~/styles/meeting.css";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesheet },
-];
-
-const meetings = [
-  {
-    id: 1,
-    name: "Leslie Alexander",
-    imageUrl:
-      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-    startDatetime: "2022-05-11T13:00",
-    endDatetime: "2022-05-11T14:30",
-  },
-  {
-    id: 2,
-    name: "Michael Foster",
-    imageUrl:
-      "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-    startDatetime: "2022-05-20T09:00",
-    endDatetime: "2022-05-20T11:30",
-  },
-  {
-    id: 3,
-    name: "Dries Vincent",
-    imageUrl:
-      "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-    startDatetime: "2022-05-20T17:00",
-    endDatetime: "2022-05-20T18:30",
-  },
-  {
-    id: 4,
-    name: "Leslie Alexander",
-    imageUrl:
-      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-    startDatetime: "2022-06-09T13:00",
-    endDatetime: "2022-06-09T14:30",
-  },
-  {
-    id: 5,
-    name: "Michael Foster",
-    imageUrl:
-      "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-    startDatetime: "2022-05-13T14:00",
-    endDatetime: "2022-05-13T14:30",
-  },
 ];
 
 function classNames(...classes: (string | boolean)[]) {
@@ -91,29 +50,32 @@ export default function Example() {
     setCurrentMonth(format(firstDayNextMonth, "MMM-yyyy"));
   }
 
-  let selectedDayMeetings = meetings.filter((meeting) =>
-    isSameDay(parseISO(meeting.startDatetime), selectedDay)
-  );
-
   return (
     <div className="min-h-screen bg-black pt-12">
       <div
-        className="max-w-md px-4 mx-auto sm:px-7 md:max-w-4xl md:px-6 p-5 rounded-lg"
+        className="max-w-md px-4 mx-auto sm:px-7 md:max-w-4xl md:px-6 rounded-lg"
         style={{
           maxWidth: "1000px",
           width: "100%",
           backgroundColor: "#1C1C1C",
+          boxSizing:'border-box',
+          border:'0.1px solid #e5e7eb'
         }}
       >
-        <div className="flex flex-col md:flex-row md:divide-x md:divide-gray-200">
+        <div className="flex flex-col md:flex-row sm:divide-x md:divide-gray-400">
           <section
-            className="mt-12 md:mt-0 md:mr-3 "
+            className="mt-12 md:mt-0 md:mr-3 pt-5"
             style={{
               maxWidth: "250px",
               width: "100%",
               backgroundColor: "#1C1C1C",
             }}
           >
+            <img
+              src={yarsa_cube}
+              alt="yarsa_logo"
+              style={{ width: "22px", height: "22px",marginBottom:'0.5rem' }}
+            />
             <p className="text-subtle text-sm font-semibold text-white">Yarsa Labs</p>
             <h1 className="text-text text-xl font-semibold my-2 text-white">
               30 Min Meeting
@@ -124,11 +86,15 @@ export default function Example() {
                   <p className="text-white">A 30 minutes meeting.</p>
                 </div>
               </div>
+
             </div>
-            
+            <p className="text-white">30 mins</p>
+            <p className="text-white">2 location options</p>
+
+
           </section>
           <div
-            className="md:pr-4 md:pl-4"
+            className="md:pr-4 md:pl-4 pt-5 pb-3"
             style={{
               maxWidth: "430px",
               width: "100%",
@@ -211,120 +177,26 @@ export default function Example() {
                   </button>
 
                   <div className="w-1 h-1 mx-auto mt-1">
-                    {meetings.some((meeting) =>
-                      isSameDay(parseISO(meeting.startDatetime), day)
-                    ) && (
-                      <div className="w-1 h-1 rounded-full bg-sky-900"></div>
-                    )}
+                   
                   </div>
                 </div>
               ))}
             </div>
           </div>
-          <section className="mt-12 md:mt-0 md:pl-5 ">
+          <section className="mt-12 md:mt-0 md:pl-5 pt-5 pb-5">
             <h2 className="font-semibold text-white">
               Schedule for{" "}
               <time dateTime={format(selectedDay, "yyyy-MM-dd")}>
                 {format(selectedDay, "MMM dd, yyy")}
               </time>
             </h2>
-            <ol className="mt-4 space-y-1 text-sm leading-6 text-gray-500">
-              {selectedDayMeetings.length > 0 ? (
-                "hello"
-              ) : (
-                // selectedDayMeetings.map((meeting) => (
-                //   <Meeting meeting={meeting} key={meeting.id} />
-                // ))
-                <p>No meetings for today.</p>
-              )}
-            </ol>
+            
           </section>
         </div>
       </div>
     </div>
   );
 }
-
-// function Meeting({ meeting }: any) {
-//   let startDateTime = parseISO(meeting.startDatetime);
-//   let endDateTime = parseISO(meeting.endDatetime);
-
-//   return (
-//     <li className="flex items-center px-4 py-2 space-x-4 group rounded-xl focus-within:bg-gray-100 hover:bg-gray-100">
-//       <img
-//         src={meeting.imageUrl}
-//         alt=""
-//         className="flex-none w-10 h-10 rounded-full"
-//       />
-//       <div className="flex-auto">
-//         <p className="text-gray-900">{meeting.name}</p>
-//         <p className="mt-0.5">
-//           <time dateTime={meeting.startDatetime}>
-//             {format(startDateTime, "h:mm a")}
-//           </time>{" "}
-//           -{" "}
-//           <time dateTime={meeting.endDatetime}>
-//             {format(endDateTime, "h:mm a")}
-//           </time>
-//         </p>
-//       </div>
-//       <Menu
-//         as="div"
-//         className={
-//           "relative opacity-0 focus-within:opacity-100 group-hover:opacity-100"
-//         }
-//       >
-//         <div>
-//           <Menu.Button className="-m-2 flex items-center rounded-full p-1.5 text-gray-500 hover:text-gray-600">
-//             <span className="sr-only">Open options</span>
-//             <DotsVerticalIcon className="w-6 h-6" aria-hidden="true" />
-//           </Menu.Button>
-//         </div>
-
-//         <Transition
-//           as={Fragment}
-//           enter="transition ease-out duration-100"
-//           enterFrom="transform opacity-0 scale-95"
-//           enterTo="transform opacity-100 scale-100"
-//           leave="transition ease-in duration-75"
-//           leaveFrom="transform opacity-100 scale-100"
-//           leaveTo="transform opacity-0 scale-95"
-//         >
-//           <Menu.Items className="absolute right-0 z-10 mt-2 origin-top-right bg-white rounded-md shadow-lg w-36 ring-1 ring-black ring-opacity-5 focus:outline-none">
-//             <div className="py-1">
-//               <Menu.Item>
-//                 {({ active }: any) => (
-//                   <a
-//                     href="#"
-//                     className={classNames(
-//                       active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-//                       "block px-4 py-2 text-sm"
-//                     )}
-//                   >
-//                     Edit
-//                   </a>
-//                 )}
-//               </Menu.Item>
-//               <Menu.Item>
-//                 {({ active }: any) => (
-//                   <a
-//                     href="#"
-//                     className={classNames(
-//                       active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-//                       "block px-4 py-2 text-sm"
-//                     )}
-//                   >
-//                     Cancel
-//                   </a>
-//                 )}
-//               </Menu.Item>
-//             </div>
-//           </Menu.Items>
-//         </Transition>
-//       </Menu>
-//     </li>
-//   );
-// }
 
 let colStartClasses = [
   "",
