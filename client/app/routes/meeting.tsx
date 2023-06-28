3801f0bd0589765d1b67881cdd9af103ba7e8193
Aniket Tamrakar <anikettamrakar0@gmail.com>
2023-06-28 00:07:51 -0700

    flexed and showed three sections in order

--- client/app/routes/meeting.tsx
+++ client/app/routes/meeting.tsx
@@ -1,6 +1,7 @@
 import { Menu, Transition } from "@headlessui/react";
 // import { DotsVerticalIcon } from "@heroicons/react/24/outline";
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/solid";
+import { LinksFunction } from "@remix-run/node";
 import {
   add,
   eachDayOfInterval,
@@ -16,6 +17,11 @@ import {
   startOfToday,
 } from "date-fns";
 import { Fragment, useState } from "react";
+import stylesheet from "~/styles/meeting.css";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesheet },
+];
 
 const meetings = [
   {
@@ -90,12 +96,47 @@ export default function Example() {
   );
 
   return (
-    <div className="pt-16">
-      <div className="max-w-md px-4 mx-auto sm:px-7 md:max-w-4xl md:px-6">
-        <div className="md:grid md:grid-cols-2 md:divide-x md:divide-gray-200">
-          <div className="md:pr-14">
+    <div className="min-h-screen bg-black pt-12">
+      <div
+        className="max-w-md px-4 mx-auto sm:px-7 md:max-w-4xl md:px-6 p-5 rounded-lg"
+        style={{
+          maxWidth: "1000px",
+          width: "100%",
+          backgroundColor: "#1C1C1C",
+        }}
+      >
+        <div className="flex flex-col md:flex-row md:divide-x md:divide-gray-200">
+          <section
+            className="mt-12 md:mt-0 md:mr-3 "
+            style={{
+              maxWidth: "250px",
+              width: "100%",
+              backgroundColor: "#1C1C1C",
+            }}
+          >
+            <p className="text-subtle text-sm font-semibold text-white">Yarsa Labs</p>
+            <h1 className="text-text text-xl font-semibold my-2 text-white">
+              30 Min Meeting
+            </h1>
+            <div className="flex items-start justify-start text-sm text-text">
+              <div className="relative z-10 mb-8 break-words max-w-full max-h-[180px] scroll-bar pr-4">
+                <div>
+                  <p className="text-white">A 30 minutes meeting.</p>
+                </div>
+              </div>
+            </div>
+            
+          </section>
+          <div
+            className="md:pr-4 md:pl-4"
+            style={{
+              maxWidth: "430px",
+              width: "100%",
+              backgroundColor: "#1C1C1C",
+            }}
+          >
             <div className="flex items-center">
-              <h2 className="flex-auto font-semibold text-gray-900">
+              <h2 className="flex-auto font-semibold text-white">
                 {format(firstDayCurrentMonth, "MMMM yyyy")}
               </h2>
               <button
@@ -115,14 +156,14 @@ export default function Example() {
                 <ChevronRightIcon className="w-5 h-5" aria-hidden="true" />
               </button>
             </div>
-            <div className="grid grid-cols-7 mt-10 text-xs leading-6 text-center text-gray-500">
-              <div>S</div>
-              <div>M</div>
-              <div>T</div>
-              <div>W</div>
-              <div>T</div>
-              <div>F</div>
-              <div>S</div>
+            <div className="grid grid-cols-7 mt-10 text-xs leading-6 text-center text-white">
+              <div>SUN</div>
+              <div>MON</div>
+              <div>TUE</div>
+              <div>WED</div>
+              <div>THU</div>
+              <div>FRI</div>
+              <div>SAT</div>
             </div>
             <div className="grid grid-cols-7 mt-2 text-sm">
               {days.map((day, dayIdx) => (
@@ -130,7 +171,7 @@ export default function Example() {
                   key={day.toString()}
                   className={classNames(
                     dayIdx === 0 && colStartClasses[getDay(day)],
-                    "py-1.5"
+                    "px-0"
                   )}
                 >
                   <button
@@ -140,23 +181,28 @@ export default function Example() {
                       isEqual(day, selectedDay) && "text-white",
                       !isEqual(day, selectedDay) &&
                         isToday(day) &&
-                        "text-red-500",
+                        "text-white",
                       !isEqual(day, selectedDay) &&
                         !isToday(day) &&
                         isSameMonth(day, firstDayCurrentMonth) &&
-                        "text-gray-900",
+                        "text-white",
+                      isEqual(day, selectedDay) &&
+                        isToday(day) &&
+                        isSameMonth(day, firstDayCurrentMonth) &&
+                        "text-black",
                       !isEqual(day, selectedDay) &&
                         !isToday(day) &&
                         !isSameMonth(day, firstDayCurrentMonth) &&
-                        "text-gray-400",
-                      isEqual(day, selectedDay) && isToday(day) && "bg-red-500",
+                        "text-gray-900",
+                      isEqual(day, selectedDay) && isToday(day) && "bg-white",
                       isEqual(day, selectedDay) &&
                         !isToday(day) &&
-                        "bg-gray-900",
-                      !isEqual(day, selectedDay) && "hover:bg-gray-200",
+                        "bg-white text-black",
+                      !isEqual(day, selectedDay) &&
+                        "button-background hover:bg-white",
                       (isEqual(day, selectedDay) || isToday(day)) &&
                         "font-semibold",
-                      "mx-auto flex h-8 w-8 items-center justify-center rounded-full"
+                      "mx-auto flex h-12 w-12 items-center justify-center rounded-lg"
                     )}
                   >
                     <time dateTime={format(day, "yyyy-MM-dd")}>
@@ -168,15 +214,15 @@ export default function Example() {
                     {meetings.some((meeting) =>
                       isSameDay(parseISO(meeting.startDatetime), day)
                     ) && (
-                      <div className="w-1 h-1 rounded-full bg-sky-500"></div>
+                      <div className="w-1 h-1 rounded-full bg-sky-900"></div>
                     )}
                   </div>
                 </div>
               ))}
             </div>
           </div>
-          <section className="mt-12 md:mt-0 md:pl-14">
-            <h2 className="font-semibold text-gray-900">
+          <section className="mt-12 md:mt-0 md:pl-5 ">
+            <h2 className="font-semibold text-white">
               Schedule for{" "}
               <time dateTime={format(selectedDay, "yyyy-MM-dd")}>
                 {format(selectedDay, "MMM dd, yyy")}
@@ -184,10 +230,11 @@ export default function Example() {
             </h2>
             <ol className="mt-4 space-y-1 text-sm leading-6 text-gray-500">
               {selectedDayMeetings.length > 0 ? (
-                selectedDayMeetings.map((meeting) => (
-                  <Meeting meeting={meeting} key={meeting.id} />
-                ))
+                "hello"
               ) : (
+                // selectedDayMeetings.map((meeting) => (
+                //   <Meeting meeting={meeting} key={meeting.id} />
+                // ))
                 <p>No meetings for today.</p>
               )}
             </ol>
@@ -198,86 +245,86 @@ export default function Example() {
   );
 }
 
-function Meeting({ meeting }: any) {
-  let startDateTime = parseISO(meeting.startDatetime);
-  let endDateTime = parseISO(meeting.endDatetime);
+// function Meeting({ meeting }: any) {
+//   let startDateTime = parseISO(meeting.startDatetime);
+//   let endDateTime = parseISO(meeting.endDatetime);
 
-  return (
-    <li className="flex items-center px-4 py-2 space-x-4 group rounded-xl focus-within:bg-gray-100 hover:bg-gray-100">
-      <img
-        src={meeting.imageUrl}
-        alt=""
-        className="flex-none w-10 h-10 rounded-full"
-      />
-      <div className="flex-auto">
-        <p className="text-gray-900">{meeting.name}</p>
-        <p className="mt-0.5">
-          <time dateTime={meeting.startDatetime}>
-            {format(startDateTime, "h:mm a")}
-          </time>{" "}
-          -{" "}
-          <time dateTime={meeting.endDatetime}>
-            {format(endDateTime, "h:mm a")}
-          </time>
-        </p>
-      </div>
-      <Menu
-        as="div"
-        className={
-          "relative opacity-0 focus-within:opacity-100 group-hover:opacity-100"
-        }
-      >
-        <div>
-          <Menu.Button className="-m-2 flex items-center rounded-full p-1.5 text-gray-500 hover:text-gray-600">
-            <span className="sr-only">Open options</span>
-            <DotsVerticalIcon className="w-6 h-6" aria-hidden="true" />
-          </Menu.Button>
-        </div>
+//   return (
+//     <li className="flex items-center px-4 py-2 space-x-4 group rounded-xl focus-within:bg-gray-100 hover:bg-gray-100">
+//       <img
+//         src={meeting.imageUrl}
+//         alt=""
+//         className="flex-none w-10 h-10 rounded-full"
+//       />
+//       <div className="flex-auto">
+//         <p className="text-gray-900">{meeting.name}</p>
+//         <p className="mt-0.5">
+//           <time dateTime={meeting.startDatetime}>
+//             {format(startDateTime, "h:mm a")}
+//           </time>{" "}
+//           -{" "}
+//           <time dateTime={meeting.endDatetime}>
+//             {format(endDateTime, "h:mm a")}
+//           </time>
+//         </p>
+//       </div>
+//       <Menu
+//         as="div"
+//         className={
+//           "relative opacity-0 focus-within:opacity-100 group-hover:opacity-100"
+//         }
+//       >
+//         <div>
+//           <Menu.Button className="-m-2 flex items-center rounded-full p-1.5 text-gray-500 hover:text-gray-600">
+//             <span className="sr-only">Open options</span>
+//             <DotsVerticalIcon className="w-6 h-6" aria-hidden="true" />
+//           </Menu.Button>
+//         </div>
 
-        <Transition
-          as={Fragment}
-          enter="transition ease-out duration-100"
-          enterFrom="transform opacity-0 scale-95"
-          enterTo="transform opacity-100 scale-100"
-          leave="transition ease-in duration-75"
-          leaveFrom="transform opacity-100 scale-100"
-          leaveTo="transform opacity-0 scale-95"
-        >
-          <Menu.Items className="absolute right-0 z-10 mt-2 origin-top-right bg-white rounded-md shadow-lg w-36 ring-1 ring-black ring-opacity-5 focus:outline-none">
-            <div className="py-1">
-              <Menu.Item>
-                {({ active }: any) => (
-                  <a
-                    href="#"
-                    className={classNames(
-                      active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-                      "block px-4 py-2 text-sm"
-                    )}
-                  >
-                    Edit
-                  </a>
-                )}
-              </Menu.Item>
-              <Menu.Item>
-                {({ active }: any) => (
-                  <a
-                    href="#"
-                    className={classNames(
-                      active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-                      "block px-4 py-2 text-sm"
-                    )}
-                  >
-                    Cancel
-                  </a>
-                )}
-              </Menu.Item>
-            </div>
-          </Menu.Items>
-        </Transition>
-      </Menu>
-    </li>
-  );
-}
+//         <Transition
+//           as={Fragment}
+//           enter="transition ease-out duration-100"
+//           enterFrom="transform opacity-0 scale-95"
+//           enterTo="transform opacity-100 scale-100"
+//           leave="transition ease-in duration-75"
+//           leaveFrom="transform opacity-100 scale-100"
+//           leaveTo="transform opacity-0 scale-95"
+//         >
+//           <Menu.Items className="absolute right-0 z-10 mt-2 origin-top-right bg-white rounded-md shadow-lg w-36 ring-1 ring-black ring-opacity-5 focus:outline-none">
+//             <div className="py-1">
+//               <Menu.Item>
+//                 {({ active }: any) => (
+//                   <a
+//                     href="#"
+//                     className={classNames(
+//                       active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+//                       "block px-4 py-2 text-sm"
+//                     )}
+//                   >
+//                     Edit
+//                   </a>
+//                 )}
+//               </Menu.Item>
+//               <Menu.Item>
+//                 {({ active }: any) => (
+//                   <a
+//                     href="#"
+//                     className={classNames(
+//                       active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+//                       "block px-4 py-2 text-sm"
+//                     )}
+//                   >
+//                     Cancel
+//                   </a>
+//                 )}
+//               </Menu.Item>
+//             </div>
+//           </Menu.Items>
+//         </Transition>
+//       </Menu>
+//     </li>
+//   );
+// }
 
 let colStartClasses = [
   "",
