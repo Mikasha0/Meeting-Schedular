1eb7a1c755ccdaa95924f877a62ab0e420bae21e
Aniket Tamrakar <anikettamrakar0@gmail.com>
2023-06-28 20:59:51 -0700

    designing meeting form section

--- client/app/routes/meeting.tsx
+++ client/app/routes/meeting.tsx
@@ -35,6 +35,7 @@ export default function Example() {
   let [selectedDay, setSelectedDay] = useState(today);
   let [currentMonth, setCurrentMonth] = useState(format(today, "MMM-yyyy"));
   let firstDayCurrentMonth = parse(currentMonth, "MMM-yyyy", new Date());
+  const [visible, setVisible] = useState(false);
 
   let days = eachDayOfInterval({
     start: firstDayCurrentMonth,
@@ -51,17 +52,19 @@ export default function Example() {
     setCurrentMonth(format(firstDayNextMonth, "MMM-yyyy"));
   }
 
+  const handleClick = () => {
+    setVisible(!visible);
+  };
+  const handleBack = () => {
+    setVisible(!visible);
+  };
+
   return (
     <div className="min-h-screen bg-black pt-12">
       <div
-        className="max-w-md px-4 mx-auto sm:px-7 md:max-w-4xl md:px-6 rounded-lg"
-        style={{
-          maxWidth: "1000px",
-          width: "100%",
-          backgroundColor: "#1C1C1C",
-          boxSizing: "border-box",
-          border: "0.1px solid #e5e7eb",
-        }}
+        className={`max-w-md px-4 mx-auto sm:px-7 md:max-w-4xl md:px-6 rounded-lg border border-gray-300 ${
+          visible === true ? "default-width" : "toggled-width"
+        }`}
       >
         <div className="flex flex-col md:flex-row sm:divide-x md:divide-gray-400">
           <section
@@ -99,104 +102,152 @@ export default function Example() {
               options
             </p>
           </section>
-          <div
-            className="md:pr-4 md:pl-4 pt-5 pb-3"
-            style={{
-              maxWidth: "430px",
-              width: "100%",
-              backgroundColor: "#1C1C1C",
-            }}
-          >
-            <div className="flex items-center">
-              <h2 className="flex-auto font-semibold text-white">
-                {format(firstDayCurrentMonth, "MMMM yyyy")}
+          {visible && (
+            <div
+              className="md:pr-4 md:pl-4 pt-5 pb-3"
+              style={{
+                maxWidth: "430px",
+                width: "100%",
+                backgroundColor: "#1C1C1C",
+              }}
+            >
+              <div className="flex items-center">
+                <h2 className="flex-auto font-semibold text-white">
+                  {format(firstDayCurrentMonth, "MMMM yyyy")}
+                </h2>
+                <button
+                  type="button"
+                  onClick={previousMonth}
+                  className="-my-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
+                >
+                  <span className="sr-only">Previous month</span>
+                  <ChevronLeftIcon className="w-5 h-5" aria-hidden="true" />
+                </button>
+                <button
+                  onClick={nextMonth}
+                  type="button"
+                  className="-my-1.5 -mr-1.5 ml-2 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
+                >
+                  <span className="sr-only">Next month</span>
+                  <ChevronRightIcon className="w-5 h-5" aria-hidden="true" />
+                </button>
+              </div>
+              <div className="grid grid-cols-7 mt-10 text-xs leading-6 text-center text-white">
+                <div>SUN</div>
+                <div>MON</div>
+                <div>TUE</div>
+                <div>WED</div>
+                <div>THU</div>
+                <div>FRI</div>
+                <div>SAT</div>
+              </div>
+              <div className="grid grid-cols-7 mt-2 text-sm">
+                {days.map((day, dayIdx) => (
+                  <div
+                    key={day.toString()}
+                    className={classNames(
+                      dayIdx === 0 && colStartClasses[getDay(day)],
+                      "px-0"
+                    )}
+                  >
+                    <button
+                      type="button"
+                      onClick={() => setSelectedDay(day)}
+                      className={classNames(
+                        isEqual(day, selectedDay) && "text-white",
+                        !isEqual(day, selectedDay) &&
+                          isToday(day) &&
+                          "text-white",
+                        !isEqual(day, selectedDay) &&
+                          !isToday(day) &&
+                          isSameMonth(day, firstDayCurrentMonth) &&
+                          "text-white",
+                        isEqual(day, selectedDay) &&
+                          isToday(day) &&
+                          isSameMonth(day, firstDayCurrentMonth) &&
+                          "text-black",
+                        !isEqual(day, selectedDay) &&
+                          !isToday(day) &&
+                          !isSameMonth(day, firstDayCurrentMonth) &&
+                          "text-gray-900",
+                        isEqual(day, selectedDay) &&
+                          isToday(day) &&
+                          "bg-white text-red-400",
+                        isEqual(day, selectedDay) &&
+                          !isToday(day) &&
+                          "bg-white text-blue-800",
+                        !isEqual(day, selectedDay) &&
+                          "button-background hover:bg-white",
+                        (isEqual(day, selectedDay) || isToday(day)) &&
+                          "font-semibold",
+                        "mx-auto flex h-12 w-12 items-center justify-center rounded-lg"
+                      )}
+                    >
+                      <time dateTime={format(day, "yyyy-MM-dd")}>
+                        {format(day, "d")}
+                      </time>
+                    </button>
+
+                    <div className="w-1 h-1 mx-auto mt-1"></div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          )}
+          {visible && (
+            <section className="mt-12 md:mt-0 md:pl-8 pt-5 pb-5">
+              <h2 className="font-semibold text-white">
+                <time dateTime={format(selectedDay, "yyyy-MM-dd")}>
+                  {format(selectedDay, "eee dd")}
+                </time>
               </h2>
               <button
                 type="button"
-                onClick={previousMonth}
-                className="-my-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
+                onClick={handleClick}
+                className="text-white hover:text-white border border-gray-300 hover:bg-gray-700 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 mt-3"
+                style={{ width: "235px" }}
               >
-                <span className="sr-only">Previous month</span>
-                <ChevronLeftIcon className="w-5 h-5" aria-hidden="true" />
+                10:30
               </button>
               <button
-                onClick={nextMonth}
                 type="button"
-                className="-my-1.5 -mr-1.5 ml-2 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
+                className="text-white hover:text-white border border-gray-300 hover:bg-gray-700 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 mt-3"
+                style={{ width: "235px" }}
               >
-                <span className="sr-only">Next month</span>
-                <ChevronRightIcon className="w-5 h-5" aria-hidden="true" />
+                11:00
               </button>
-            </div>
-            <div className="grid grid-cols-7 mt-10 text-xs leading-6 text-center text-white">
-              <div>SUN</div>
-              <div>MON</div>
-              <div>TUE</div>
-              <div>WED</div>
-              <div>THU</div>
-              <div>FRI</div>
-              <div>SAT</div>
-            </div>
-            <div className="grid grid-cols-7 mt-2 text-sm">
-              {days.map((day, dayIdx) => (
-                <div
-                  key={day.toString()}
-                  className={classNames(
-                    dayIdx === 0 && colStartClasses[getDay(day)],
-                    "px-0"
-                  )}
-                >
-                  <button
-                    type="button"
-                    onClick={() => setSelectedDay(day)}
-                    className={classNames(
-                      isEqual(day, selectedDay) && "text-white",
-                      !isEqual(day, selectedDay) &&
-                        isToday(day) &&
-                        "text-white",
-                      !isEqual(day, selectedDay) &&
-                        !isToday(day) &&
-                        isSameMonth(day, firstDayCurrentMonth) &&
-                        "text-white",
-                      isEqual(day, selectedDay) &&
-                        isToday(day) &&
-                        isSameMonth(day, firstDayCurrentMonth) &&
-                        "text-black",
-                      !isEqual(day, selectedDay) &&
-                        !isToday(day) &&
-                        !isSameMonth(day, firstDayCurrentMonth) &&
-                        "text-gray-900",
-                      isEqual(day, selectedDay) && isToday(day) && "bg-white",
-                      isEqual(day, selectedDay) &&
-                        !isToday(day) &&
-                        "bg-white text-black",
-                      !isEqual(day, selectedDay) &&
-                        "button-background hover:bg-white",
-                      (isEqual(day, selectedDay) || isToday(day)) &&
-                        "font-semibold",
-                      "mx-auto flex h-12 w-12 items-center justify-center rounded-lg"
-                    )}
-                  >
-                    <time dateTime={format(day, "yyyy-MM-dd")}>
-                      {format(day, "d")}
-                    </time>
-                  </button>
+            </section>
+          )}
+          {!visible &&  <section className="mt-12 md:mt-0 md:pl-6 pt-5 pb-5">
+            {/* <div className="mb-2 flex items-center">
+              <label className="text-emphasis mb-2 block text-sm font-medium !mb-0">
+                <span className="text-white">Your name</span>
+                <span className="text-emphasis -mb-1 ml-1 text-sm font-medium leading-none text-white">
+                  *
+                </span>
+              </label>
 
-                  <div className="w-1 h-1 mx-auto mt-1"></div>
-                </div>
-              ))}
+
+            </div> */}
+            <div>
+              <label
+                htmlFor="first_name"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Your name *
+              </label>
+              <input
+                type="text"
+                id="first_name"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                style={{ width: "370px" }}
+                placeholder="John"
+                required
+              />
+              <button onClick={handleBack}>Back</button>
             </div>
-          </div>
-          <section className="mt-12 md:mt-0 md:pl-5 pt-5 pb-5">
-            <h2 className="font-semibold text-white">
-              <time dateTime={format(selectedDay, "yyyy-MM-dd")}>
-                {format(selectedDay, "eee dd")}
-              </time>
-
-            </h2>
-            <button type="button" className="text-white hover:text-white border border-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">Default</button>
-
-          </section>
+          </section>}
+         
         </div>
       </div>
     </div>
--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -2,13 +2,9 @@ import type { Config } from 'tailwindcss'
 
 export default {
   
-  content: ['./app/**/*.{js,jsx,ts,tsx}',
-  "./node_modules/flowbite-react/**/*.js"
-],
+  content: ['./app/**/*.{js,jsx,ts,tsx}',],
   theme: {
     extend: {},
   },
-  plugins: [
-    require("flowbite/plugin")
-],
+  plugins: [],
 } satisfies Config
