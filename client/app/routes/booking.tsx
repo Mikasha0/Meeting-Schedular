d13c2de0932703fa74473178a343f05dfe6fdd82
Aniket Tamrakar <anikettamrakar0@gmail.com>
2023-07-03 21:55:22 -0700

    prev date when selected throw error

--- client/app/routes/booking.tsx
+++ client/app/routes/booking.tsx
@@ -1,8 +1,14 @@
+import { LinksFunction, LoaderArgs } from "@remix-run/node";
 import { BsCheckCircleFill } from "react-icons/bs";
+import stylesheet from "~/styles/meeting.css";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesheet },
+];
 
 export default function booking() {
   return (
-    <div className="min-h-screen bg-[#f3f4f6] pt-8">
+    <div className="min-h-screen bg-[#f3f4f6] pt-8 mb-3">
       <div
         className={`max-w-md p-8 mx-auto sm:px-7 md:max-w-4xl md:px-6 rounded-lg border border-gray-300 `}
         style={{
@@ -13,31 +19,31 @@ export default function booking() {
         <div className=" mx-auto flex justify-center">
           <BsCheckCircleFill style={{ fill: "green" }} size={40} />
         </div>
-        <h3 className="text-emphasis text-2xl font-semibold leading-6 text-center mt-5">
+        <h3 className="text-2xl font-semibold leading-6 text-center mt-5">
           This meeting is scheduled
         </h3>
 
         <div className="mt-3">
-          <p className="text-default text-center">
+          <p className="text-center">
             We emailed you and the other attendees a calendar invitation with
             all the details.
           </p>
         </div>
-        <div className="border-subtle text-default mt-8 grid grid-cols-3 border-t pt-8 text-left">
-          <div className="font-medium">What</div>
+        <div className=" mt-8 grid grid-cols-3 border-t pt-8 text-left">
+          <div className="font-semibold">What</div>
           <div className="col-span-2 mb-6 last:mb-0">
             30 Min Meeting between Yarsa Labs and Aniket Tamrakar
           </div>
-          <div className="font-medium">When</div>
+          <div className="font-semibold">When</div>
           <div className="col-span-2 mb-6 last:mb-0">
-            <div className="">
+            <div>
               Wednesday, July 5, 2023
               <br />
               12:00 AM - 12:30 AM{" "}
-              <span className="text-bookinglight">(Pacific Daylight Time)</span>
+              {/* <span className="text-bookinglight">(Pacific Daylight Time)</span> */}
             </div>
           </div>
-          <div className="font-medium">Who</div>
+          <div className="font-semibold">Who</div>
           <div className="col-span-2 last:mb-0">
             <div className="mb-3">
               <div>
@@ -55,10 +61,24 @@ export default function booking() {
               </p>
             </div>
           </div>
-          <div className="mt-3 font-medium">Where</div>
+          <div className="mt-3 font-semibold">Where</div>
           <div className="col-span-2 mt-3">Yarsa Labs Office</div>
         </div>
-        <div className="mt-7"></div>
+        <hr className="mt-7" />
+        <p className="text-default mt-7 text-center">
+          Need to make a change?{" "}
+          <a
+            href="/meeting"
+            className="underline"
+            style={{ marginRight: "5px" }}
+          >
+            Reschedule
+          </a>
+          or
+          <a href="#cancel" className="underline" style={{ marginLeft: "5px" }}>
+            Cancel
+          </a>
+        </p>
       </div>
     </div>
   );
--- client/app/routes/meeting.tsx
+++ client/app/routes/meeting.tsx
@@ -32,63 +32,70 @@ function classNames(...classes: (string | boolean)[]) {
   return classes.filter(Boolean).join(" ");
 }
 
-
 export const action = async ({ request }: ActionArgs) => {
   const form = await request.formData();
-  const { name, email, location,notes, guests } = getMeetingFormData(form);
+  const { name, email, location, notes, guests } = getMeetingFormData(form);
   console.log(name, email, location, notes, guests);
   const params = new URLSearchParams(request.url.split("?")[1]);
-  const time = params.get("time")
-  const date = params.get("date")
-//   const fieldErrors = validateMeetingForm(name, location, email);
-//   console.log(fieldErrors)
-//   const hasErrors = !Object.values(fieldErrors).every(
-//     (value) => value === undefined
-//   );
-  
-//   if (hasErrors) {
-//     return badRequest({
-//       fieldErrors,
-//       fields: null,
-//       formError: "Form not submitted correctly",
-//     });
-//   }
-//   const fields= {
-//       name,
-//       email,
-//       location,
-  
-//     };
+  const time = params.get("time");
+  const date = params.get("date");
+  const parseResult = meetingSchema.safeParse({
+    time,
+    email,
+    date,
+    name,
+    location,
+    notes,
+    guests,
+  });
 
-// if (Object.values(fieldErrors).some(Boolean)) {
-//   return badRequest({
-//     fieldErrors,
-//     fields,
-//     formError: null,
-//   });
-// }
-  const parseResult = (meetingSchema.safeParse({time, email, date, name, location,notes,guests}))
   if (!parseResult.success) {
-    const fieldErrors = parseResult.error.format()
-    console.log(fieldErrors)
+    const fieldErrors = parseResult.error.format();
+    console.log(fieldErrors);
     return badRequest({
       fieldErrors,
       fields: null,
-      formError: 'Form not submitted correctly'
+      formError: "Form not submitted correctly",
     });
-    }
+  }
 
-  return redirect("/booking");
+  const API_URL = "http://localhost:3333/api/meeting";
+  const req = {
+    "name": "John Doe",
+    "email": "johndoe@gmail.com",
+    "location": "Yarsa Meet",
+    "notes": "notes",
+    "guests": [
+      "user1@gmail.com",
+      "user2@gmail.com"
+    ],
+    "time": "11:00",
+    "date": "2023/07/11"
+  }
+  try {
+    const response = await fetch(API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(parseResult.data),
+    });
+    console.log(await response.json());
+  } catch (error) {
+    // Handle any exceptions during the API request
+    console.log("API request error:", error);
+    return new Response("API request error", { status: 500 });
+  }
 };
+
 export default function Meeting() {
   let navigate = useNavigate();
   let today = startOfToday();
   const actionData = useActionData<typeof action>();
-  const timeValues = Object.values(ACCEPTED_TIME)
+  const timeValues = Object.values(ACCEPTED_TIME);
   let [selectedDay, setSelectedDay] = useState(today);
   let [currentMonth, setCurrentMonth] = useState(format(today, "MMM-yyyy"));
   const [visible, setVisible] = useState(false);
-
 
   let firstDayCurrentMonth = parse(currentMonth, "MMM-yyyy", new Date());
 
@@ -116,7 +123,7 @@ export default function Meeting() {
       <div
         className={`max-w-md px-4 mx-auto sm:px-7 md:max-w-4xl md:px-6 rounded-lg  ${
           visible === true ? "default-width" : "toggled-width"
-        }`} 
+        }`}
       >
         <div className="flex flex-col md:flex-row sm:divide-x ">
           <section
@@ -205,11 +212,11 @@ export default function Meeting() {
                     <button
                       type="button"
                       onClick={() => {
-                        setSelectedDay(day)
-                       }}
+                        setSelectedDay(day);
+                      }}
                       className={classNames(
                         isEqual(day, selectedDay) && "text-white",
-                        !isEqual(day, selectedDay) && //todays date 
+                        !isEqual(day, selectedDay) && //todays date
                           isToday(day) &&
                           "text-red-400",
                         !isEqual(day, selectedDay) &&
@@ -256,26 +263,37 @@ export default function Meeting() {
                 </time>
               </h2>
               {timeValues.map((time) => (
-            <button
-              key={time}
-              type="button"
-              className="text-black hover:text-white border border-gray-300 hover:bg-gray-700 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-0 mt-3"
-              style={{ width: "235px" }}
-              onClick={() => {
-                console.log(days)
-                const year = selectedDay.getFullYear().toString()
-                const month = (selectedDay.getMonth() + 1).toString()
-                const day1 = selectedDay.getDate()
-                const date = year + '/' + month + '/' + day1
-
-                navigate(`/meeting/?time=${time}&date=${date}`)
-                setVisible(!visible)
-                // Handle button click event here
-              }}
-            >
-              {time}
-            </button>
-          ))}
+         <button
+         key={time}
+         type="button"
+         className="text-black hover:text-white border border-gray-300 hover:bg-gray-700 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-0 mt-3"
+         style={{ width: "235px" }}
+         onClick={() => {
+           const currentDate = new Date(); // Get the current date
+           const selectedDate = new Date(selectedDay); // Convert the selectedDay to a Date object
+       
+           // Compare the selected date with the current date
+           if (selectedDate < currentDate) {
+             // Show an error message or handle the error condition here
+             console.log("Error: Selected date is in the past");
+             return;
+           }
+       
+           console.log(days);
+           const year = selectedDay.getFullYear().toString();
+           const month = (selectedDay.getMonth() + 1).toString();
+           const day1 = selectedDay.getDate();
+           const date = year + "/" + month + "/" + day1;
+       
+           navigate(`/meeting/?time=${time}&date=${date}`);
+           setVisible(!visible);
+           // Handle button click event here
+         }}
+       >
+         {time}
+       </button>
+       
+              ))}
               {/* <button
                 type="button"
                 onClick={handleClick}
@@ -291,10 +309,11 @@ export default function Meeting() {
               >
                 11:00
               </button> */}
-
             </section>
           )}
-          {visible && <MeetingForm handleClick={handleClick} actionData={actionData}/>}
+          {visible && (
+            <MeetingForm handleClick={handleClick} actionData={actionData} />
+          )}
         </div>
       </div>
     </div>
@@ -310,5 +329,3 @@ let colStartClasses = [
   "col-start-6",
   "col-start-7",
 ];
-
-
